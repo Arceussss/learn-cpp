67f4786b891d858ba387286437a2e249f17fd735
Danbert Macugay <danbertmacugaydmm8@gmail.com>
2025-12-19 01:33:50 +0800

    Add basic subtraction example

---
+++ basic-subtraction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first_number;
+    int second_number;
+
+    cout << "Enter 1st number: ";
+    cin >> first_number;
+
+    cout << "Enter 2nd number: ";
+    cin >> second_number;
+
+    cout << "Difference = " << (first_number - second_number) << endl;
+    return 0;
+}
+
